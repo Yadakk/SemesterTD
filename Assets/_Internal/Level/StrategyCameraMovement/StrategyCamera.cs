6980d382f850423e163b7f90ed35289931be84fc
Yad <lolyer47@gmail.com>
2024-09-16 15:00:58 +0300

    feat: Added camera panning

---
+++ Assets/_Internal/Level/StrategyCameraMovement/StrategyCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StrategyCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 0.01f;
+
+    private StrategyCameraActions actions;
+
+    private void Awake()
+    {
+        actions = new();
+        actions.Main.OnPan.started += OnPanHandler;
+    }
+
+    private void OnEnable()
+    {
+        actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actions.Disable();
+    }
+
+    private void OnPanHandler(InputAction.CallbackContext context)
+    {
+        Vector3 horizontalRotationEuler = transform.rotation.eulerAngles;
+        horizontalRotationEuler.x = 0f;
+        Quaternion horizontalRotation = Quaternion.Euler(horizontalRotationEuler);
+
+        Vector2 delta = context.ReadValue<Vector2>();
+
+        Vector3 transformDelta = horizontalRotation * new Vector3(delta.x, 0f, delta.y);
+        transform.position += transformDelta * sensitivity;
+    }
+}
