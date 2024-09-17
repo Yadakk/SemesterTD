3757f26537d0b5f4a3248a09b2f8a038223e2841
Yad <lolyer47@gmail.com>
2024-09-17 17:28:51 +0300

    feat: Units can now move on roads

--- Assets/_Internal/Level/Units/Unit.cs
+++ Assets/_Internal/Level/Units/Unit.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 1f;
 
+    private UnitPathNode currentNode;
+    private UnitPathNode targetNode;
+
+    private Vector3 currentNodePosition;
+    private Vector3 targetNodePosition;
+
+    private float nodeDistance;
+
+    private float nodeProgress01;
+
+    private void Update()
+    {
+        if (nodeProgress01 >= 1f)
+        {
+            SetCurrentNode(targetNode);
+        }
+
+        transform.position = Vector3.Lerp(currentNodePosition, targetNodePosition, nodeProgress01);
+        nodeProgress01 += Time.deltaTime / nodeDistance * speed;
+    }
+
+    public void SetCurrentNode(UnitPathNode node)
+    {
+        nodeProgress01 = 0f;
+
+        currentNode = node;
+        targetNode = GetNextNode(node);
+
+        currentNodePosition = currentNode.transform.position;
+        currentNodePosition.y = transform.position.y;
+
+        targetNodePosition = targetNode.transform.position;
+        targetNodePosition.y = transform.position.y;
+
+        nodeDistance = Vector3.Distance(currentNodePosition, targetNodePosition);
+    }
+
+    private UnitPathNode GetNextNode(UnitPathNode node)
+    {
+        return node.ConnectedNodes[Random.Range(0, node.ConnectedNodes.Count)];
+    }
 }
